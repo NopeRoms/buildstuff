13af6422c01068d3a09cc0d5906d6a024234efe6
NopeRoms <133530462+NopeRoms@users.noreply.github.com>
2023-06-10 15:14:13 +0530

    build los for real this time

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 python --version
-time repo init -u https://github.com/PixelExperience/manifest -b thirteen-plus --depth=1 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NopeNopeGuy/local_manifests.git .repo/local_manifests -b ruby
+time repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --depth=1 --git-lfs -g default,-mips,-darwin,-notdefault
 echo "repo init"
 time repo sync -j32 -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync || repo sync -j4 -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 git lfs install
 repo forall -c 'git lfs pull'
 echo "sync complete"
-rm -rf vendor/aosp
-rm -rf packages/apps/Settings
-rm -rf packages/apps/Updates
-git clone https://github.com/NopeNopeGuy/packages_apps_Settings packages/apps/Settings -b thirteen-plus
-git clone https://github.com/NopeNopeGuy/packages_apps_Updates packages/apps/Updates -b thirteen
-git clone https://github.com/NopeNopeGuy/vendor_aosp vendor/aosp -b thirteen-plus
+git clone https://github.com/NopeNopeGuy/device_xiaomi_whyred device/xiaomi/whyred --depth=1 -b thirteen-lineage
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_whyred.git vendor/xiaomi/whyred --depth=1
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sdm660-common.git vendor/xiaomi/sdm660-common --depth=1
+git clone https://github.com/NopeNopeGuy/device_xiaomi_sdm660-common.git device/xiaomi/sdm660-common --depth=1 -b thirteen-lineage
+git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_sdm660.git kernel/xiaomi/sdm660 --depth=1
+git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi --depth=1
+rm -rf packages/modules/Permission
+rm -rf frameworks/base
+git clone https://github.com/NopeNopeGuy/android_frameworks_base.git frameworks/base --depth=1 -b lineage-20.0
+git clone https://github.com/NopeNopeGuy/android_packages_modules_Permission.git packages/modules/Permission --depth=1 -b lineage-20.0
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -20,4 +23,4 @@ ccache -M 30G
 ccache -o compression=true 
 ccache -z
 export ALLOW_MISSING_DEPENDENCIES=true
-timeout 90m bash -c "source build/envsetup.sh && lunch aosp_whyred-userdebug && mka bacon -j16" || exit 0
+timeout 90m bash -c "source build/envsetup.sh && lunch lineage_whyred-userdebug && mka bacon -j16" || exit 0
